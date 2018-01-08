b967d8d1fae47da98ce2f5bf7482d351c508c260
Vakhtin Andrey <VakhtinAndrey@users.noreply.github.com>
2018-01-09 01:40:37 +0300

    * Started working with file pathes, constants and forms * Misc refactoring

---
+++ savefiledecoder/Consts.cs
@@ -0,0 +1,20 @@
+﻿namespace savefiledecoder
+{
+    public static class Consts
+    {
+        public static class EpisodeStates
+        {
+            public const string InProgress = "kInProgress";
+            public const string Finished = "kFinished";
+            public const string NotPlayed = "kNotPlayed";
+            public const string Unavailable = "kUnavailable";
+        }
+
+        public static class GlobalCodes
+        {
+            public const string GlobalCodeReadyToStartEpisode = "GLOBAL_CODE_READYTOSTARTEPISODE";
+            public const string GlobalCodeStoryComplete = "GLOBAL_CODE_STORYCOMPLETE";
+            public const string GlobalCodeSaveJustStarted = "GLOBAL_CODE_SAVEJUSTSTARTED";
+        }
+    }
+}
--- savefiledecoder/MainForm.Designer.cs
+++ savefiledecoder/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace savefiledecoder
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
@@ -602,7 +602,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LiS BtS Savegame Editor";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.tableLayoutPanel4.ResumeLayout(false);
             this.tableLayoutPanelEpisodes.ResumeLayout(false);
